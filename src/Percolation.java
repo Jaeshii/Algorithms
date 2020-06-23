b88a5b13f0bc0c88459a501728dd1577c699c131
jaeshii <yinj5927@gmail.com>
2020-06-22 23:11:34 -0700

    updated Percolation.java and PercolationStats.java

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,57 +1,55 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.util.Arrays;
-
 public class Percolation {
-    WeightedQuickUnionUF gridConnections;
-    int[][] grid;
-    final int N;
+    private WeightedQuickUnionUF gridConnections;
+    private boolean[][] grid;
+    private final int n;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) throws IllegalArgumentException {
+    public Percolation(int n) {
         if (n <= 0)
             throw new IllegalArgumentException("n must be greater than 0");
-        N = n;
-        gridConnections = new WeightedQuickUnionUF((int)(Math.pow(n, 2) + 2));
-        grid = new int[n][n];
+        this.n = n;
+        gridConnections = new WeightedQuickUnionUF((int) (Math.pow(n, 2) + 2));
+        grid = new boolean[n][n];
 
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) throws IllegalArgumentException {
-        if ((row > N || row < 0) || (col > N || col < 0))
+    public void open(int row, int col) {
+        if ((row > n || row <= 0) || (col > n || col <= 0))
             throw new IllegalArgumentException("not a valid row or column");
-        if (! isOpen(row, col)) {
-            grid[row - 1][col - 1] = 1;
+        else if (!isOpen(row, col)) {
+            grid[row - 1][col - 1] = true;
             // connect to virtual top or bottom site if in topmost or bottommost row
             if (row == 1) {
                 gridConnections.union(gridLoc(row, col), 0);
-            } else if (row == N) {
-                gridConnections.union(gridLoc(row, col), (int) (Math.pow(N, 2)) + 1);
+            } else if (row == n) {
+                gridConnections.union(gridLoc(row, col), (int) (Math.pow(n, 2)) + 1);
             }
             // connect adjacent open tiles
             if (row - 1 > 0 && isOpen(row - 1, col))
                 gridConnections.union(gridLoc(row - 1, col), gridLoc(row, col));
-            if (row + 1 < N + 1 && isOpen(row + 1, col))
+            if (row + 1 < n + 1 && isOpen(row + 1, col))
                 gridConnections.union(gridLoc(row + 1, col), gridLoc(row, col));
             if (col - 1 > 0 && isOpen(row, col - 1))
                 gridConnections.union(gridLoc(row, col - 1), gridLoc(row, col));
-            if (col + 1 < N + 1 && isOpen(row, col + 1))
+            if (col + 1 < n + 1 && isOpen(row, col + 1))
                 gridConnections.union(gridLoc(row, col + 1), gridLoc(row, col));
         }
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col) throws IllegalArgumentException {
-        if ((row > N || row < 0) || (col > N || col < 0))
+    public boolean isOpen(int row, int col) {
+        if ((row > n || row < 0) || (col > n || col < 0))
             throw new IllegalArgumentException("not a valid row or column");
 
-        return grid[row - 1][col - 1] == 1;
+        return grid[row - 1][col - 1] == true;
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) throws IllegalArgumentException {
-        if ((row > N || row < 0) || (col > N || col < 0))
+    public boolean isFull(int row, int col) {
+        if ((row > n || row < 0) || (col > n || col < 0))
             throw new IllegalArgumentException("not a valid row or column");
 
         return gridConnections.find(gridLoc(row, col)) == gridConnections.find(0);
@@ -60,9 +58,9 @@ public class Percolation {
     // returns the number of open sites
     public int numberOfOpenSites() {
         int openNum = 0;
-        for (int row = 0; row < N; row++) {
-            for (int col = 0; col < N; col++) {
-                if (grid[row][col] == 1)
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == true)
                     openNum++;
             }
         }
@@ -71,27 +69,27 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return gridConnections.find((int) (Math.pow(N, 2)) + 1) == gridConnections.find(0);
+        return gridConnections.find((int) (Math.pow(n, 2)) + 1) == gridConnections.find(0);
     }
 
     // returns the index given (row, col)
     private int gridLoc(int row, int col) {
-        return N * (row - 1) + col;
+        return n * (row - 1) + col;
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation perc = new Percolation(5);
-        for (int i = 0; i < perc.grid.length; i++)
-            System.out.println(Arrays.toString(perc.grid[i]));
-        perc.open(1, 1);
-        perc.open(2, 1);
-        perc.open(3, 1);
-        perc.open(4, 1);
-        perc.open(5, 1);
-        for (int i = 0; i < perc.grid.length; i++)
-            System.out.println(Arrays.toString(perc.grid[i]));
-        System.out.println(perc.percolates());
-        System.out.println(perc.numberOfOpenSites());
+//        Percolation perc = new Percolation(5);
+//        for (int i = 0; i < perc.grid.length; i++)
+//            System.out.println(Arrays.toString(perc.grid[i]));
+//        perc.open(1, 1);
+//        perc.open(2, 1);
+//        perc.open(3, 1);
+//        perc.open(4, 1);
+//        perc.open(5, 1);
+//        for (int i = 0; i < perc.grid.length; i++)
+//            System.out.println(Arrays.toString(perc.grid[i]));
+//        System.out.println(perc.percolates());
+//        System.out.println(perc.numberOfOpenSites());
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -2,23 +2,22 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private double[] trials;
-
+    private static final double CONFIDENCE_95 = 1.96;
+    private final double[] trials;
 
     // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials) throws IllegalArgumentException {
+    public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0)
             throw new IllegalArgumentException("invalid n or number of trials");
 
         this.trials = new double[trials];
         for (int trial = 0; trial < trials; trial++) {
             Percolation perc = new Percolation(n);
-            int numOpen = 0;
-            while (! perc.percolates()) {
+            while (!perc.percolates()) {
                 perc.open(StdRandom.uniform(1, n + 1), StdRandom.uniform(1, n + 1));
-                numOpen++;
             }
-            this.trials[trial] = (double) (numOpen / (int) (Math.pow(n, 2)));
+
+            this.trials[trial] = ((double) perc.numberOfOpenSites() / (int) (Math.pow(n, 2)));
         }
     }
 
@@ -34,20 +33,20 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return (mean() - 1.96 * stddev()) / (Math.sqrt(trials.length));
+        return (mean()) - (CONFIDENCE_95 * stddev() / (Math.sqrt(trials.length)));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return (mean() + 1.96 * stddev()) / (Math.sqrt(trials.length));
+        return (mean()) + (CONFIDENCE_95 * stddev() / (Math.sqrt(trials.length)));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        String s1 = "2";
-        String s2 = "1000";
+//        String s1 = "2";
+//        String s2 = "1000";
 
-        PercolationStats test = new PercolationStats(Integer.parseInt(s1), Integer.parseInt(s2));
+        PercolationStats test = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println("mean                    = " + test.mean());
         System.out.println("stddev                  = " + test.stddev());
         System.out.println("95% confidence interval = [" + test.confidenceLo() + ", " + test.confidenceHi() + "]");
