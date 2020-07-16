d9516dc993104a1f57f2cbaa9f37ac690c96f5d5
Jaeshii <56745535+Jaeshii@users.noreply.github.com>
2020-07-16 01:45:15 -0700

    added Ball, BouncingBalls, Particle, and CollisionSystem

---
+++ src/Ball.java
@@ -0,0 +1,25 @@
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Ball {
+    private double rx, ry;        // position
+    private double vx, vy;        // velocity
+    private final double radius;  // radius
+    public Ball() {
+        rx = Math.random();
+        ry = Math.random();
+        vx = Math.random() / 10;
+        vy = Math.random() / 10;
+        radius = 0.02;
+
+    }
+
+    public void move(double dt) {
+        if ((rx + vx*dt < radius) || (rx + vx*dt > 1.0 - radius)) { vx = -vx; }
+        if ((ry + vy*dt < radius) || (ry + vy*dt > 1.0 - radius)) { vy = -vy; }
+        rx = rx + vx*dt;
+        ry = ry + vy*dt;
+    }
+    public void draw() {
+        StdDraw.filledCircle(rx, ry, radius);
+    }
+}
